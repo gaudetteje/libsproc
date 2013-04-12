c26587276a2df134c64aeed522766a53c4a08535
Jason Gaudette <jason_gaudette@brown.edu>
2013-04-12 14:40:48 -0400

    Updates to various functions

--- beampatterns/pistonbeam.m
+++ beampatterns/pistonbeam.m
@@ -3,15 +3,15 @@
 
 % piston transducer beam pattern
 
-F = (20e3:10e3:100e3).';
+F = 10e3;%(10e3:15e3:100e3).';
 c = 344;
 lambda = c./F;
-theta = (-90:90);
+theta = (-90:.1:90);
 
 
 
 % piston transducer beam (diameter D in infinite baffle)
-D = 0.0047*2;
+D = .02; %.025;%0.0047*2;
 beta = sin(theta*pi/180);
 B1 = (2 * besselj(1,(pi*D./lambda) * beta) ./ (pi*D./lambda * beta)).^2;
 
@@ -21,13 +21,13 @@ axis([-90 90 -40 0])
 grid
 
 
-% 
-% % line array of n elements (spacing of d)
-% n = 2;
-% d = .014;
-% B2 = (sin(n*pi*d*sin((theta*pi/180) ./lambda)) ./ ...
-%       (n*sin((pi*d./lambda)*sin(theta*pi/180))) ).^2;
-% 
-% figure
-% plot(theta,db(B2))
-% grid
+
+% line array of n elements (spacing of d)
+n = 1;
+d = .014;
+B2 = (sin(n*pi*d*sin((theta*pi/180) ./lambda)) ./ ...
+      (n*sin((pi*d./lambda)*sin(theta*pi/180))) ).^2;
+
+figure
+plot(theta,db(B2))
+grid
